3890a950461fe08be57326bc5f495ceb0bbc6282
L-Steinmacher <dasai.sacul@gmail.com>
2021-03-09 12:35:07 -0800

    set up app

---
+++ git_app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './App.css';
+
+class App extends React.Component{
+  // state = {
+
+  // }
+
+  render() {
+    return(
+      <div>
+        <h1>I'm alive</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
