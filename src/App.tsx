81eba27ee90fccc89ada0b42422e5b1746ad5165
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-08 21:00:02 -0500

    fetch user data using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react'
+import axios from 'axios'
 
 interface AppProps {
 
 }
 
 interface AppState {
-
+  user: {
+    username: string
+    avatar: string
+    url: string
+    location: string
+  } | null
 }
+
+const BASE_URL = 'https://api.github.com/users'
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: { }) {
     super(props)
 
     this.state = {
-
+      user: null
     }
+  }
+
+  componentDidMount() {
+    axios.get(`${BASE_URL}/lindellcarternyc`)
+      .then(res => {
+        const { login, avatar_url, html_url, location } = res.data
+        this.setState({
+          user: {
+            username: login,
+            avatar: avatar_url,
+            url: html_url,
+            location
+          }
+        })
+      })
+      .catch(err => {
+        console.error(err)
+      })
   }
 
   render() {
     return (
-      <div>App</div>
+      <div>
+        <h1>Github User Card</h1>
+        {JSON.stringify(this.state.user)}
+      </div>
     )
   }
 }
