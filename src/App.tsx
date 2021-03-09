007932be5779286d49aa65a3c36878d3dcbd36ba
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-09 11:42:54 -0500

    fetch follower data update interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,22 @@ interface AppProps {
 
 }
 
+interface User {
+  username: string
+  avatar: string
+  url: string
+  location: string
+  followers: Follower[]
+}
+
+interface Follower {
+  username: string
+  avatar: string
+  url: string
+}
+
 interface AppState {
-  user: {
-    username: string
-    avatar: string
-    url: string
-    location: string
-  } | null
+  user: User | null
 }
 
 const BASE_URL = 'https://api.github.com/users'
@@ -29,14 +38,29 @@ class App extends React.Component<AppProps, AppState> {
     axios.get(`${BASE_URL}/lindellcarternyc`)
       .then(res => {
         const { login, avatar_url, html_url, location } = res.data
-        this.setState({
-          user: {
-            username: login,
-            avatar: avatar_url,
-            url: html_url,
-            location
-          }
-        })
+        const user: User = {
+          username: login,
+          avatar: avatar_url,
+          url: html_url,
+          location,
+          followers: []
+        }
+
+        axios.get(`${BASE_URL}/lindellcarternyc/followers`)
+          .then(res => {
+            res.data.forEach((u: any) => {
+              const follower: Follower = {
+                username: u.login,
+                url: u.html_url,
+                avatar: u.avatar_url
+              }
+              user.followers.push(follower)
+            })
+            this.setState({ user })
+          })
+          .catch(err => {
+            console.error(err)
+          })
       })
       .catch(err => {
         console.error(err)
