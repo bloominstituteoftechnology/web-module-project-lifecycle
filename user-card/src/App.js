ab4c535c754c34139314427cbe4d2bbf7865e707
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-03-09 13:33:17 -0800

    user input box created

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -5,18 +5,38 @@ import axios from 'axios'
 
 class App extends React.Component{
     state = {
-      user: "monicascz",
+      user: [],
+      userInput: "",
     }
 
     componentDidMount(){
+      axios.get(`https://api.github.com/users/monicascz`)
+        .then((resp)=>{
+          // console.log(resp.data)
+          this.setState({
+            user: resp.data,
+          })
+        })
+        .catch((err)=>{
+          console.log('error', err)
+        })
+    }
 
+    handleChange= (e)=>{
+      this.setState({
+        userInput: e.target.value
+      })
     }
 
   render(){
     return(
       <>
         <h1>Git Hub User Card</h1>
-        <UserCard/>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" name="userInput" value={this.state.userInput} onChange={this.handleChange}></input>
+          <button>Find My Card</button>
+        </form>
+        <UserCard user={this.state.user}/>
       </>
     )
   }
--- user-card/src/Component/UserCard.js
+++ user-card/src/Component/UserCard.js
@@ -4,7 +4,21 @@ class UserCard extends React.Component {
 
     render(){
         return(
-           <h2> Here is where a card would go</h2>
+           <div className="userCard">
+               <h1>{this.props.user.name} </h1>
+               <div className="image">
+                    <img width="200px" src={this.props.user.avatar_url} alt={this.props.user.name}/>
+               </div>
+               <div className="details">
+                   <h3>{this.props.user.login}</h3>
+                   <p>Location: {this.props.user.location}</p>
+                   <p>Profile: 
+                       <a href={this.props.user.html_url}> {this.props.user.html_url}</a>
+                   </p>
+                   <p>Followers: {this.props.user.followers}</p>
+                   <p>Following: {this.props.user.following}</p>
+               </div>
+           </div>
         )
     }
 
