03cc4886542ab2b30b869d0c0f4aee4ce3a2b922
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-09 19:42:34 -0500

    use FollowersList component

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,33 +1,50 @@
+import { makeStyles, } from '@material-ui/core'
+
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Link from '@material-ui/core/Link'
+import Typography from '@material-ui/core/Typography'
+
+import FollowersList from './FollowersList'
+
 import { User } from '../models/User.model'
 
 interface UserCardProps {
     user: User
 }
 
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 300,
+        margin: `0 auto`
+    },
+    location: {
+        marginTop: '1rem'
+    }
+})
+
 const UserCard: React.FC<UserCardProps> = ({ user }) => {
+    const classes = useStyles()
+
     return (
-        <article>
-            <header>
-                <h3>
-                    <a href={user.url} target="__blank" rel="noopener norefferer">{user.username}</a>
-                </h3>
-                <img src={user.avatar} alt="user avatar" />
-            </header>
-            <section>
-                <ul>
-                    {user.followers.map((follower, idx) => {
-                        return (
-                            <li key={idx}>
-                                <div>
-                                    <p>{follower.username}</p>
-                                    <img src={follower.avatar} alt="follower avatar" />
-                                </div>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </section>
-        </article>
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography variant="h5" gutterBottom>
+                    <Link color="inherit" href={user.url} target="_blank" rel="noopener noreferrer">
+                        {user.username}
+                    </Link>
+                </Typography>
+                <CardMedia 
+                    src={user.avatar}
+                    component="img"
+                />
+                <Typography className={classes.location}>
+                    Location: {user.location}
+                </Typography>
+                <FollowersList followers={user.followers} />
+            </CardContent>
+        </Card>
     )
 }
 
