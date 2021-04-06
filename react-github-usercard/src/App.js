822c13d19d4f787c0e66ecfced22cee963ab06e3
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-04-06 15:55:12 -0400

    Remade app into a constructor function

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -1,20 +1,46 @@
+import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-axios.get('https://api.github.com/users/wzd200')
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+// Getting Feet Wet
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the Github Usercard Application</h1>
-    </div>
-  );
+// axios.get('https://api.github.com/users/wzd200')
+//   .then(res => {
+//     const gitData = res
+//     console.log(gitData)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Welcome to the Github Usercard Application</h1>
+//     </div>
+//   );
+// }
+
+// Using Class Components //
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <h1>
+         Welcome to the Github Usercard Application
+        </h1>
+      </div>
+    )
+  }
+
 }
 
 export default App;
