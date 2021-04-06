98cc020a28e44998ca265baea576138ed5502407
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-04-06 15:42:06 -0400

    Performed successful axios get with my own github user profile

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -1,5 +1,13 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+
+axios.get('https://api.github.com/users/wzd200')
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 function App() {
   return (
