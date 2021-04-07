85f7097815cfbf9d965a30305d922d02ac55a1c8
Devon Osei <oseidevon@gmail.com>
2021-04-06 23:17:47 -0400

    yolo

---
+++ app-name/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+constructor(){
+  super();
+  this.state = {
+    userPic: '',
+    followers: 0,
+    id:''
+  }
+}
+
+  componentDidMount() {
+    // make an api call
+    axios.get("https://api.github.com/users/OseiDevon")
+      .then(res => {
+        console.log(res.data.avatar_url)
+        this.setState({
+          ...this.state,
+          userPic: res.data.avatar_url,
+          followers: res.data.followers,
+          id:res.data.id
+        })
+      })
+      .catch(err => console.error('Error: ', err.message))
+  }
+
+
+  render() {
+    return (<>
+      <div>
+       <img key={this.state.id} src={this.state.userPic} alt="user pic" />
+       <p>{`Followers: ${this.state.followers}`}</p>
+     </div>
+    </>)
+  }
+}
+
+export default App;
