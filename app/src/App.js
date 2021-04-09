3f3b32f136a68da78a786e5d4ebeda4fb427f1aa
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-04-08 21:19:28 -0400

    UserCard

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,42 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import UserCard from "./components/UserCard"
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      user: [],
+    };
   }
 
   componentDidMount() {
     axios
-      .get("https://api.github.com/users/SJMucho/followers")
-      .then((res) => console.log(res.data.message))
+      .get("https://api.github.com/users/SJMucho")
+      .then((res) => {
+        this.setState({ ...this.state, user: res.data.message });
+      })
       .catch((err) => err);
+  }
+
+  fetchUser = () => {
+
+  }
+
+  componentDidUpdate() {
+
   }
 
   render() {
     return (
       <div>
         <p>Hello</p>
+        <input type="text" /><button onClick={this.fetchUser}>Click Me</button>
+        {this.state.user.map(user => {
+          return <img src={user} key={user} />;
+        })}
+        <UserCard />
       </div>
     );
   }
