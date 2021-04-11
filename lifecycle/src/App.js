d239f0c0178b6b25ae92c4674bbaa91d16ab800e
orlando taylor <ackerie_taylor@hotmail.com>
2021-04-11 17:34:10 -0400

    work in progress

---
+++ lifecycle/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import axios from 'axios'
+import Card from './component/Card'
+
+class App extends React.Component {
+  // eslint-disable-next-line
+  constructor() {
+    super();
+      this.state = {
+        newUser: '',
+        user: ''
+           }
+     }
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users/Ackerie')
+    .then(responce => this.setState({...this.state, user:responce.data}))
+    .catch(err => console.log(err, 'opps somethings not right'))
+  }
+  
+  
+  
+  getUser = () => {
+  axios.get(`https://api.github.com/users/${this.state.newUser}`)
+    .then(responce => this.setState({...this.state, user:responce.data}))
+    .catch(err => console.log(err, 'opps somethings not right'))
+  }
+
+  handlechanges = e => {
+    this.setState({
+      ...this.state,
+      newUser: e.target.value
+    })
+    
+  }
+
+   render() {
+     return(
+       <div>
+         <h1>GitHub UserCards</h1>
+          <input 
+          type="text"
+          value={this.state.newUser}
+          onChange={this.handlechanges}
+          />
+          <button onClick={this.getUser}>Get UserCards</button>
+
+         
+
+          </div>
+          
+            
+         
+       
+     )
+   }
+}
+export default App;
