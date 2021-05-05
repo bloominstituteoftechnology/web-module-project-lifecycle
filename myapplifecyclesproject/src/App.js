b330be5aa824c74062619ef03fc66c136d7b3ff5
Zach-Morris <zach-morris@lambdastudents.com>
2021-05-05 00:19:28 -0400

    CRA Built

---
+++ myapplifecyclesproject/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
+import Card from './Card'
+ 
+
+class App extends React.Component {
+    state = {
+        users: [],
+        following: [],
+    };
+
+    componentDidMount () {
+        axios.get('https://api.github.com/users/zach-morris-txt')
+            .then((response) => {
+              this.setState({
+                users: response.data ,
+              });            
+            })
+            .catch((error) => console.log(error));
+        axios.get('https://api.github.com/users/zach-morris-txt/followers')
+            .then((response) => {
+              this.setState({
+                following: response.data ,            
+              });
+            })
+            .catch((error) => console.log(error));
+    }
+
+    render() {
+        console.log('rendering');
+        return (
+            <div className='App'>
+                <Card users={this.state.users} followers={this.state.followers} />                
+            </div>
+        );
+    };
+}
+
+export default App;
---
+++ myapplifecyclesproject/src/Card.js
@@ -0,0 +1,11 @@
+
+
+const Card = (props) => {
+    return (
+      <div>
+        <h1>Name: {props.users.name}</h1>
+      </div>
+    );
+  };
+
+  export default Card;
