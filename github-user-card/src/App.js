977d7a6cae004000e508f0dd16d53dbdb96d3a99
John Vehmeyer <j.vehmeyer@gmail.com>
2021-06-01 15:08:59 -0500

    rendering user info to screen

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,15 +1,63 @@
+import axios from 'axios';
 import React from 'react';
 import './App.css';
+import UserCard from './components/UserCard';
+
 
 class App extends React.Component {
+  // Create state
+  state = {
+     users: []
+  }
+
+  // Axios requests 
+  // https://api.github.com/users/Vehmeyer
+  // https://api.github.com/users/Vehmeyer/followers
+  
+  componentDidMount() {
+    axios.get("https://api.github.com/users/vehmeyer")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          users: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  // Create handler functions
+
+  // handleChange = e => {
+  //   this.setState({
+  //     users: e.target.value
+  //   });
+  // }
+
+  // handleClick = e => {
+  //   e.preventDefault();
+  //   axios.get("https://api.github.com/users/Vehmeyer/followers")
+  //     .then(res => {
+
+  //     })
+  //     .catch(err => {
+  //       console.log(err)
+  //     })
+  // }
+
   render() {
+      console.log(this.state.users);
+      console.log(this.state.users.name);
+
       return (
       <div className="App">
         <header className="App-header">
-            <h1>GitHub User Card</h1>
+          <h1>GitHub User Card</h1>
         </header>
         <div className="userContainer">
-        
+          <UserCard users={this.state.users}/>
+          {/* <h3>{this.state.users.name}</h3> */}
         </div>
       </div>
     );
--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,4 +1,20 @@
 import React from 'react';
 
+const UserCard = props => {
+    return(
+        <div className="CardContainer">
+            <div>
+                <img src={props.users.avatar_url} />
+            </div>
+            <h2>{props.users.name}</h2>
+            <p>Username: {props.users.login}</p>
+            <p>{props.users.location}</p>
+            <p>Followers: {props.users.followers}</p>
+            <p>Following: {props.users.following}</p>
+            <p>{props.users.bio}</p>
+
+        </div>
+    )
+}
 
 export default UserCard;
