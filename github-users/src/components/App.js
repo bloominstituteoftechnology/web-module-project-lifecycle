8fb1ec0c737b96edc22a524a8fe1aed5ea7b3c9c
Spencerp34 <spencerp34@yahoo.com>
2021-07-06 16:33:10 -0600

    finished MVP

--- github-users/src/components/App.js
+++ github-users/src/components/App.js
@@ -5,40 +5,70 @@ import axios from 'axios';
 class App extends React.Component {
 
   state= {
+    user: 'spencerp34',
     URL: '',
     pic: '',
     name: '',
-    followers: 0    
+    followers: []    
   }
 
   componentDidMount(prevProps, prevState) {
-    axios.get('https://api.github.com/users/spencerp34')
+    axios.get(`https://api.github.com/users/${this.state.user}`)
       .then(res => {
         this.setState({
           ...this.state,
-          URL: res.data.url,
+          URL: res.data.html_url,
           pic: res.data.avatar_url,
           name: res.data.name,
-          followers: res.data.followers
         })
       })
       .catch(err => {
         console.log(err)
       })
+
+      axios.get(`https://api.github.com/users/${this.state.user}/followers`)
+        .then(res => {
+          this.setState({
+            ...this.state,
+            followers: res.data
+          })
+        })
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
 
-    console.log('submitted')
+    // console.log('submitted')
+    axios.get(`https://api.github.com/users/${this.state.user}`)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          URL: res.data.html_url,
+          pic: res.data.avatar_url,
+          name: res.data.name,
+        })
+      })
 
+      axios.get(`https://api.github.com/users/${this.state.user}/followers`)
+        .then(res => {
+          this.setState({
+            ...this.state,
+            followers: res.data
+          })
+        })
 
-    var form = document.getElementById("myForm");
-    form.reset();
+      var form = document.getElementById("myForm");
+      form.reset();
   }
 
+  
+
   handleChange = (e) => {
-    console.log(e.target.value)
+    // console.log(e.target.value)
+    this.setState({
+      ...this.state,
+      user: e.target.value
+    })
   }
 
   render(){
@@ -51,6 +81,22 @@ class App extends React.Component {
             <button>Lets Stalk!</button>
           </form>
         </header>
+        <br/> <br/>
+        <div className='user-card'>
+          <img src={this.state.pic} alt='pic'/>
+          <br/>
+          <p>
+            Name: {this.state.name}
+          </p>
+          <nav>
+            <a href={this.state.URL}>{`URL: ${this.state.URL}`}</a>
+          </nav>
+          <p>
+            Followers: {this.state.followers.map(item => {
+              return(`${item.login}, `) 
+            })}
+          </p>
+        </div>
 
       </div>
     );
