924cc9b57ec9c3a291c59f3a452625738aed1b2f
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-07-10 14:53:29 -0700

    Made request for my own github and my followers. Getting data back

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -1,14 +1,45 @@
-
+import React from 'react'
+import axios from 'axios'
+import UserCard from './Components/UserCard'
+import FollowerCard from './Components/FollowerCard'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-     
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      user: {},
+      followers: []
+    }
+  }
+  componentDidMount() {//user
+    axios
+    .get('https://api.github.com/users/cynthia-coronado')
+    .then(response => {
+      this.setState({...this.state, user: response.data})
+      console.log(response)
+    })
+    .catch(error => {
+      console.log('User Error')
+    })
+  
+  axios //follower
+    .get('https://api.github.com/users/cynthia-coronado/followers')
+    .then(response => {
+      this.setState({...this.state, followers: response.data})
+      console.log(response);
+    })
+    .catch(error => {
+      console.log('Follower Error')
+    })
+ }
+
+render() {
+  return (
+    <div className = 'appContainer'>
+    <h1>Github</h1>
+    </div>
+  )
+ }
+}
