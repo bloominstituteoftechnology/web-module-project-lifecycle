d6a3fd25fecd94a76d7d709633b387b96c9b22c8
Angela Engel <amengel.ae@icloud.com>
2021-07-11 14:00:32 -0600

    MVP

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 import axios from 'axios'
 
@@ -11,10 +12,50 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/amengel18`)
+    .then(res => {
+      this.setState({usercards: res.data});
+    })
+    axios.get('https://api.github.com/users/amengel18/followers')
+      .then(res => {
+        console.log(res.data);
+        this.setState({ followers: res.data });
+    })
+      .catch(err => {console.log(err);
+    });
+  }
+
   render() {
+
+    const myStyle = {
+      width: "100%",
+      padding: '20px',
+      display: 'flex',
+      justifyContent: 'center',
+      border: '5px',
+      boxShadow: '0 1px 6px -2px #000',
+      backgroundColor: '#6E0B77',
+      marginBottom: '30px'
+    }
+
     return (
-      <div>
+      <div className="App-header">
         <h2>My Usercard</h2>
+        <div>
+          <h3 style={myStyle}>{this.state.usercards.name}</h3>
+          <h3 style={myStyle}>{this.state.usercards.bio}</h3>
+        </div>
+        <div>
+        <h3>Follower Count: {this.state.usercards.followers}</h3>
+				<h3>Followers:</h3>
+				{this.state.followers.map((follower, i) => {
+					return <h4 key={i}>{follower.login}</h4>;
+				})}
+				<h3>Following: {this.state.usercards.following}</h3>
+				<h3>Public Repos: {this.state.usercards.public_repos}</h3>
+				<h3>Git Repo: {this.state.usercards.html_url}</h3>
+        </div>
       </div>
     )
   }
