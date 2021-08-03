e7b24a3e3e82bb95cce054e01a4ed8f069d5d409
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-08-03 14:21:01 -0700

    initialcommit

---
+++ user-card/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './App.css';
+import axios from 'axios'
+
+class App extends React.Component {
+  state = {
+    userData :{},
+    followers: {}
+  }
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users/VAIBHAVIBALAR')
+    .then(res =>{
+      console.log(res)
+      this.setState({
+        ...this.state,
+        userData: res.data
+      })
+      console.log(this.state.userData)
+    })
+    axios.get('https://api.github.com/users/VAIBHAVIBALAR/followers')
+    .then(res =>{
+      res.data.map(item =>{
+        console.log(item)
+        this.setState({
+          ...this.state,
+          followers: item
+        })
+      })
+      
+    })
+  }
+  render() {
+    let data = this.state.userData
+    console.log(data)
+    let users = this.state.followers
+    console.log(users)
+    return(<div className="App">
+        <h1>Git User Card</h1>
+          <div>
+          <div><h1>Avataar</h1><img src={data.avatar_url}/></div>
+          <div><h2>Bio</h2>{data.bio}</div>
+          <div><h2>Location</h2>{data.location}</div>
+          <div><h2>Followers</h2>{data.followers}</div>
+          <div><h2>Followers avatar</h2><img src={users.avatar_url}/></div>
+          <div><h2>Followers Login</h2>{users.login}</div>
+          <div><h2>Followers followers</h2>{users.followers}</div>
+          <div><h2>Followers type</h2>{users.type}</div>
+       </div> 
+      </div>)
+  }
+}
+export default App;
