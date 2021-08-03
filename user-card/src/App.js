be970910d4049fcbc76745a59437891de1a99af0
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-08-03 14:39:19 -0700

    finalstylingcommit

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -36,13 +36,15 @@ class App extends React.Component {
     let users = this.state.followers
     console.log(users)
     return(<div className="App">
-        <h1>Git User Card</h1>
-          <div>
-          <div><h1>Avataar</h1><img src={data.avatar_url}/></div>
+        <h1 className="title">Git User Card</h1>
+          <div className="Card">
+          <div><h1>VAIBHAVIBALAR</h1><img src={data.avatar_url}/></div>
           <div><h2>Bio</h2>{data.bio}</div>
           <div><h2>Location</h2>{data.location}</div>
           <div><h2>Followers</h2>{data.followers}</div>
-          <div><h2>Followers avatar</h2><img src={users.avatar_url}/></div>
+          </div>
+          <div className="Followers">
+          <div><h2>Followers avatar</h2><img className="img" src={users.avatar_url}/></div>
           <div><h2>Followers Login</h2>{users.login}</div>
           <div><h2>Followers followers</h2>{users.followers}</div>
           <div><h2>Followers type</h2>{users.type}</div>
