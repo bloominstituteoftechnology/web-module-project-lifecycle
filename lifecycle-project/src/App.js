9c5efd1b628c7880162eb5be8484082f60c50ddb
Klove Adams <kalebsxott@gmail.com>
2021-08-03 15:55:02 -0400

    starting

---
+++ lifecycle-project/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>Stuff go's here</p>
+      </div>
+    );
+  }
+}
+
+export default App;
