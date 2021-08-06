684499a20711d175b0939bcfec28896780050a31
Bimisola Daniel <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-08-06 04:15:39 +0100

    Making progress

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import axios from "axios";
+import User from "./Components/User";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Github User Card</h1>
+      <User />
+    </div>
+  );
+}
+
+export default App;
