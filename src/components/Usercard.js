5fc61fbd9d2075682a89566fa9d43c337250a9d1
Mykael Barnes <64759782+Mykeb96@users.noreply.github.com>
2021-08-06 18:58:05 -0700

    simple changes

--- src/components/Usercard.js
+++ src/components/Usercard.js
@@ -18,11 +18,11 @@ class UserCard extends React.Component {
         return (
             <div>
             <img src={this.props.avatar} />
-            <div>{this.props.name}</div>
-            <div>{this.props.login}</div>
-            <div>{this.props.id}</div>
-            <div>{this.props.url}</div>
-            <div>{this.props.followers}</div>
+            <div>Name: {this.props.name}</div>
+            <div>Login: {this.props.login}</div>
+            <div>ID: {this.props.id}</div>
+            <div>Github URL: {this.props.url}</div>
+            <div>Follower URL: {this.props.followers}</div>
             <div>Follower count: {this.props.followerCount}</div>
             </div>
         )
