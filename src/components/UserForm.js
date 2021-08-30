eb0d4bd5f63ca0337954478d48c7a075c263c89d
Rick <rmansfield@realpeoplesratings.com>
2021-08-30 17:19:07 -0500

    set up app.js added base UserForm.js connectetd'em

---
+++ src/components/UserForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class userForm extends Component {
+    render() {
+        return (
+            <div>
+                <input />
+                <button>Add User</button>
+            </div>
+        );
+    }
+}
+
+export default userForm;
