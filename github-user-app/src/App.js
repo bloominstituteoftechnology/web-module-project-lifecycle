2950f2c8368bf46d53392548af3bbc7b6e9a0c24
Stuart Darsey <stuart.darsey@gmail.com>
2021-08-31 15:31:08 -0400

    removed etc CRA things. header and form built

---
+++ github-user-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+  
+
+  render () {
+    return (
+      <div className='app-container'>
+        <h1>Github User App</h1>
+          <form>
+            <input />
+            <button>Search Github User</button>
+          </form>
+      </div>
+    );
+  };
+};
+
+export default App;
