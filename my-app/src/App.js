78096262405d9a4f64312919fdd7d8d02ade64c7
Richard Oh <n24512@protonmail.com>
2021-08-31 14:23:57 -0500

    rendering own card and followers cards to DOM

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
+import Follower from './Follower';
 import './App.css';
 
 class App extends React.Component {
-
   state = {
-      myCard: 'https://api.github.com/users/ayv8er',
-      followersCards: [
-    'https://api.github.com/users/tetondan',
-    'https://api.github.com/users/dustinmyers',
-    'https://api.github.com/users/justsml',
-    'https://api.github.com/users/luishrd',
-    'https://api.github.com/users/bigknell',
-    'https://api.github.com/users/LeonelHays',
-    'https://api.github.com/users/onuri5',
-    'https://api.github.com/users/TraNequaFauntleroy',
-    'https://api.github.com/users/christina-yun',
-    'https://api.github.com/users/krisf451',
-    'https://api.github.com/users/Raj-04',
-    'https://api.github.com/users/patricelachelle',
-    'https://api.github.com/users/Pluviio',
-    'https://api.github.com/users/vannguyen141290',
-    'https://api.github.com/users/vibhasjain',
-    'https://api.github.com/users/jeffyjkang',
-    'https://api.github.com/users/cyberkade',
-    'https://api.github.com/users/justinfineberg',
-    'https://api.github.com/users/SomeiLam'
-    ]
+      myCard: '',
+      followersCards: []
   }
 
+  componentDidMount() {
+    axios.get('https://api.github.com/users/ayv8er')
+      .then(res => {
+        // console.log(res)
+        this.setState({
+          ...this.state,
+          myCard: res.data
+        })
+        axios.get(res.data['followers_url'])
+          .then(response => {
+            // console.log(response)
+            this.setState({
+              ...this.state,
+              followersCards: response.data
+            })
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   render() {
     return (
@@ -36,8 +41,17 @@ class App extends React.Component {
       <header><h1>Richard's Github Information</h1></header>
         <div className='myCard'>
           <div className='myPic'>
-            <img src="https://avatars.githubusercontent.com/u/84942969?v=4" />
+            <img src={this.state.myCard.avatar_url} alt={this.state.myCard.login}/>
           </div>
+        </div>
+
+        <div className='followers'>
+          {
+            this.state.followersCards.map(item => {
+              console.log(item)
+              return <Follower key={item.id} follower={item} />
+            })
+          }
         </div>
       </>
     )
