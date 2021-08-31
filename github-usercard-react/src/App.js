d265f04be8341b32358004a2934d255af4d15b6e
nicole pizzano <pizzano.nicole@gmail.com>
2021-08-31 11:52:32 -0400

    Save work

--- github-usercard-react/src/App.js
+++ github-usercard-react/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+ import axios from 'axios';
+ import UserCard from './components/UserCard';
+ import FollowerCard from './components/FollowerCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ class App extends React.Component {
+   constructor() {
+     super();
+     this.state = {
+       user: [],
+     }
+   }
 
-export default App;
+
+   // login: "AidanJJenkins"
+   // followers: 2
+   // avatar_url: "https://avatars.githubusercontent.com/u/75285874?v=4"
+
+   componentDidMount() {
+     axios.get("https://api.github.com/users/WrongAlice")
+       .then(res => this.setState({
+         ...this.state,
+         user: res.data
+       }))
+       .catch(err => console.log(err))
+   }
+
+
+   render() {
+     return (
+       <div className="App">
+         <h1>UserCard project</h1>
+         <UserCard login={this.state.user.login} followers={this.state.user.followers} avatar={this.state.user.avatar_url}/>
+         <FollowerCard />
+       </div>
+     );
+   }
+ }
+
+ export default App; 
