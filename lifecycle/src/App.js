7661f413416b72a65bc5f3a309c6f5d3f35ecea8
Sebastian <sebas6292@gmail.com>
2021-08-31 17:26:59 -0700

    oh yeah

---
+++ lifecycle/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import axios from 'axios';
+
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Sebastian's Card</h1>
+          <form>
+            <input/>
+            <button>Find friend</button>
+            <div id='data'>
+
+            </div>
+            
+          </form>
+        </div>
+      )
+    }
+}
+     
+export default App;
