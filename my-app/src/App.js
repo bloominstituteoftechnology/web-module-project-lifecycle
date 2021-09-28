42ddf3376a02967f33fa6285619af2f78c8dd999
minasoha <minasoha@yahoo.com>
2021-09-28 17:36:33 -0400

    added dinamic images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,19 @@ import axios from 'axios';
 
 class App extends React.Component{
   render(){
+    const followers = [
+      "https://avatars.githubusercontent.com/u/1483837?v=4",
+      "https://avatars.githubusercontent.com/u/16849092?v=4",
+      "https://avatars.githubusercontent.com/u/39303588?v=4",
+      "https://avatars.githubusercontent.com/u/71340015?v=4",
+      "https://avatars.githubusercontent.com/u/75883540?v=4",
+      "https://avatars.githubusercontent.com/u/84058769?v=4",
+      "https://avatars.githubusercontent.com/u/86445750?v=4",
+      "https://avatars.githubusercontent.com/u/87157585?v=4",
+      "https://avatars.githubusercontent.com/u/87495756?v=4",
+      "https://avatars.githubusercontent.com/u/87831982?v=4",
+    ]
+      
     return(
       <div>
         <h1>Welcome to my Github</h1>
@@ -12,7 +25,14 @@ class App extends React.Component{
           <img src="https://avatars.githubusercontent.com/u/87757115?v=4" />
           <p>Username: minasoha</p>
           <p>Follower Count: 10</p>
-          
+        </div>
+        <div>
+          {
+            followers.map(followers => {
+              return(<img key={followers} width="200" src={followers} />)
+            })
+
+          }
         </div>
       </div>
     )
