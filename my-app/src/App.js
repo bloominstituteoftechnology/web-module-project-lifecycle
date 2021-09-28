d73e99da443dfb739f747dfbe074184b5aa380af
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2021-09-28 16:22:20 -0500

    React App created

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+          <h1 className='App-logo'>My app here</h1>
+      </header>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
