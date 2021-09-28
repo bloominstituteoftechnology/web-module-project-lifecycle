ce3c0e299cb99843d034c999193067a891370818
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-09-28 16:57:19 -0700

    started working on App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component 
+{
+    state =
+        {
+            users: [],
+            user: ""
+        };
+
+    componentDidMount()
+    {
+        axios.get("https://api.github.com/users/anastasia-lapteva")
+            .then(resp =>
+            {
+                this.setState({
+                    ...this.state,
+                    users: resp.data.login
+                });
+            });
+    };
+
+    handleInput = (e) =>
+    {
+        this.setState({
+            ...this.state,
+            user: e.target.name
+        });
+    };
+
+    render()
+    {
+        return (
+            <div>
+                <header>
+                    <h1>React Github User Card</h1>
+                </header>
+                <form>
+                    <h2>TEST</h2>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
