4e2df2e41148fb45deb05c0aea805eab3f26bf8c
minasoha <minasoha@yahoo.com>
2021-09-28 19:39:49 -0400

    added some styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,53 +7,61 @@ class App extends React.Component{
 
   state = {
     followers: [],
-   
-    
+    search: ""
   }
-
 
     componentDidMount(){
       axios.get('https://api.github.com/users/minasoha/followers')
-        .then(resp =>{
-          
+        .then(resp =>{         
           this.setState({
             ...this.state,
-            followers: resp.data.map(res =>{
-              
-              return{
-                
-                login: res.login,
-                picture: res.avatar_url
-                
-              }
-              
-            })
-            
+            followers: resp.data.map(res =>{              
+              return{                
+                login: res.html_url,
+                picture: res.avatar_url,
+                             
+              }              
+            })       
           })
         })
-
-
     }
+    handleInput = e =>{
+      this.setState({
+        ...this.state, 
+        search: e.target.value
+      })
+    }
+    handleSearch = e => {
+      e.preventDefault();
+
+      axios.get(`https://github.com/${this.state.search}`)
+      .then(resp =>{
+        console.log(resp)
+      })
+    }
+
+
   render(){
     
       
     return(
       <div>
         <h1>Welcome to my Github</h1>
-        <div className="my-github">
+        <div className="myGithub">
           <img src="https://avatars.githubusercontent.com/u/87757115?v=4" />
           <p>Username: minasoha</p>
           <p>Follower Count: 10</p>
         </div>
-        <form>
-          <input />
-          <button> Search</button>
-          <div className="followers">
+        <form className="followers">
+          <input value={this.state.search} onChange={this.handleInput}/>
+          <button onClick={this.handleSearch}> Search</button>
+          <div className="pics">
             {  this.state.followers.map(followers => {
                 return(
                 <div>
                   <img key={followers} width="200" src={followers.picture} />
                   <p>{followers.login}</p>
+                  
                 </div>
                 )
               })
