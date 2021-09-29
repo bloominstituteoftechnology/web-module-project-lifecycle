c4ec2b4c753bc46c0c329904dfee52d7f4efc04a
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2021-09-29 01:45:44 -0500

    still in progress

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,65 @@
-
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className='App-header'>
-          <h1 className='App-logo'>My app here</h1>
-      </header>
-      <div>
+class App extends React.Component
+{
+    state = {
+        followers: '',
+        name: ''
+    };
 
-      </div>
-    </div>
-  );
+
+    componentDidMount(handleInput)
+    {
+        axios.get(`https://api.github.com/users/${this.state.name}`)
+
+            .then(resp =>
+            {
+                console.log('Component mounting:', resp);
+                this.setState({
+                    ...this.state,
+                    followers: resp.data.followers_url
+                });
+            });
+    }
+
+    handleInput = (e) =>
+    {
+        this.setState({
+            ...this.state,
+            name: e.target.value
+        });
+    };
+
+    // handleSearch = (e) =>
+    // {
+    //     e.preventDefault();
+
+    //     axios.get(`https://https://api.github.com/users/${this.state.name}/followers`)
+    //         .then(resp =>
+    //         {
+    //             this.setState({
+    //                 ...this.state,
+    //                 followers: resp.data.followers_url
+    //             });
+    //         });
+    // };
+    render()
+    {
+        return (
+            <div className="App">
+                <header className='App-header'>
+                    <h1 className='App-logo'>{this.name}</h1>
+                </header>
+                <nav className="nav-button">
+                    <input value={this.state.name} onChange={this.handleInput} />
+                    <button className='nav-button' onClick={this.handleInput}>Find Git Hub User</button>
+                </nav>
+
+            </div>
+        );
+    }
 }
 
 export default App;
