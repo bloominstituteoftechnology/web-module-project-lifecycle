1d0d5d28bc59d186dcd5a1ce18bb49232f6a4a11
Angelique Abacajan <ma_utsey@yahoo.com>
2021-10-26 16:44:15 -0400

    Implemented App.js and User.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React from 'react'
+import axios from 'axios'
+import User from './components/User'
+import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      githubData: {
+      name: '',
+      login: '',
+      avatar_url: '',
+      location: '',
+      bio: '',
+      html_url: '',
+      followers: '',
+      following: '',
+      }
+    }
+    console.log('Github data rendered')
+  }
+
+  componentDidMount() {
+    console.log('Component Mounted')
+    axios.get('https://api.github.com/users/MaryAngelique')
+        .then(res => this.setState({
+            ...this.state,
+            githubData: res.data
+        }))
+        .catch(err => console.log(err));
+  }
+
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return (
+      <div className="App">
+        <h1>{this.state.githubData.name}'s Github Profile:</h1>
+        <User userData={this.state.githubData}/>
+      </div>
+    );
   }
 }
 
