13ce53644e0b506f418d2d5a8f20d70fb4f84154
Justin Peisker <justinpeisker@gmail.com>
2021-10-26 11:49:41 -0700

    DOM done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+      <div>
+        <h1> GitHub Info </h1>
+        <input />
+        <button>Search</button>    
+        <div className= 'main-profile'>
+          <img src= 'https://avatars.githubusercontent.com/u/87906218?v=4' alt='' />
+          <h2> Name: </h2>
+          <h4> Total Repos: </h4>
+          <h4> Total Followers: </h4>
+        </div>
+        <div className= 'followers'>
+          <img src= '' alt= '' />
+          <h3> Name: </h3>
+
+        </div>
+
+      </div>);
   }
 }
 
