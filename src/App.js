04c29437f3cc1f00fa46698414e259790e961252
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-26 17:30:32 -0400

    static into to DOM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return (
+      <div>
+        <h1>Github Card</h1>
+        <form>
+          <input placeholder="GitHub Username" />
+          <button>Search</button>
+        </form>
+        <div className="user-profile">
+          <img src="https://avatars.githubusercontent.com/u/89364961?v=4" />
+          <h2>Tina Gao</h2>
+          <h5>username: tinaxgao</h5>
+          <h4>Repos: 37</h4>
+          <h4>Followers: 18</h4>
+        </div>
+        <h3>Followers</h3>
+        <div className="followers-list">
+          <div className="follower-card">
+            <img src="https://avatars.githubusercontent.com/u/89364961?v=4" />
+            <h4>First Last</h4>
+          </div>
+          <div className="follower-card">
+            <img src="https://avatars.githubusercontent.com/u/89364961?v=4" />
+            <h4>First Last</h4>
+          </div>
+          <div className="follower-card">
+            <img src="https://avatars.githubusercontent.com/u/89364961?v=4" />
+            <h4>First Last</h4>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
