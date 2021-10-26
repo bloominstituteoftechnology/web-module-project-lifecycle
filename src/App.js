3310e1e1963ca246523818678a192762ce0b99e9
valera1900 <alex.olaras11@gmail.com>
2021-10-26 18:04:43 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -6,14 +6,38 @@ class App extends Component {
     gitHubUser: {},
   };
   componentDidMount() {
-    fetch("https://api.github.com/users/valera1900")
+    fetch("https://api.github.com/users/fabpot")
       .then((response) => response.json())
       .then((json) => this.setState({ gitHubUser: json }));
   }
 
   render() {
     console.log(this.state.gitHubUser);
-    return <div className="App"></div>;
+    return (
+      <>
+        <div className="container">
+          <div className="about">
+            <img src={this.state.gitHubUser.avatar_url} alt="" />
+            <p>{this.state.gitHubUser.name}</p>
+            <p>{this.state.gitHubUser.login}</p>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Sponsor</button>
+            </div>
+            <div className="media">
+              <p>{this.state.gitHubUser.followers} followers • </p>
+              <p>{this.state.gitHubUser.following} following</p>
+            </div>
+            <div className="info">
+              <p>{this.state.gitHubUser.company}</p>
+              <p>{this.state.gitHubUser.location}</p>
+              <p>{this.state.gitHubUser.blog}</p>
+              <p>{this.state.gitHubUser.twitter_username}</p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
