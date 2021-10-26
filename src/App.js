4d67aa2876189a059730754e3d02fdc963eeb063
Justin Peisker <justinpeisker@gmail.com>
2021-10-26 12:40:50 -0700

    first api call wired up

--- src/App.js
+++ src/App.js
@@ -3,17 +3,47 @@ import axios from 'axios';
 
 
 class App extends React.Component {
+
+  state= {
+    avatar_url: '',
+    name: '',
+    login: '',
+    public_repos: '',
+    followers: '',
+
+  }
+
+  componentDidMount () {
+    axios.get("https://api.github.com/users/justinpeisker")
+      .then(res => {
+        console.log(res);
+        this.setState({
+          ...this.state,
+          avatar_url: res.data.avatar_url,
+          name: res.data.name,
+          login: res.data.login,
+          public_repos: res.data.public_repos,
+          followers: res.data.followers
+        })
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
   render() {
     return(
       <div>
         <h1> GitHub Info </h1>
-        <input />
-        <button>Search</button>    
+        <form>
+          <input />
+          <button>Search</button>    
+        </form>
         <div className= 'main-profile'>
-          <img src= 'https://avatars.githubusercontent.com/u/87906218?v=4' alt='' />
-          <h2> Name: </h2>
-          <h4> Total Repos: </h4>
-          <h4> Total Followers: </h4>
+          <img src= {this.state.avatar_url} alt='' />
+          <h2> {this.state.name} </h2>
+          <p> {this.state.login} </p>
+          <h4> Total Repos: {this.state.public_repos} </h4>
+          <h4> Total Followers: {this.state.followers} </h4>
         </div>
         <div className= 'followers'>
           <img src= '' alt= '' />
