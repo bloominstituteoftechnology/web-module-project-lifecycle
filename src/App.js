9e657efb3ec6ac4a9c3e4b697dfa6192de3d5247
Peter Conley <peterdavidconley@gmail.com>
2021-10-26 14:24:45 -0400

    created static structure for application

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
+    return(
+    <div>
+      <section>
+        <h1>GITHUB INFO</h1>
+        <button>Github Handle</button>
+        <button>Search</button>
+      </section>
+      <br/>
+      <section>
+        <img height='250px' width='250px' src='https://avatars.githubusercontent.com/u/85258866?v=4'/>
+        <div>
+          <h3>PETER CONLEY</h3>
+          <p>(peterdavidconley)</p>
+          <h4>TOTAL REPOS: </h4>
+          <h4>TOTAL FOLLOWERS:</h4>
+        </div>
+      </section>
+      <section>
+        <h2>FOLLOWERS:</h2>
+        <br />
+        <img height='150px' width='150px' src='https://avatars.githubusercontent.com/u/18486822?v=4'/>
+        <h5>nullflux</h5>
+      </section>
     </div>);
   }
 }
