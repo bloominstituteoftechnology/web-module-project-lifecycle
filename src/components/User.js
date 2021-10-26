78a037817134abb5040e0eb022d4c979cde62175
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-26 17:52:20 -0400

    add user  profile

--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 
-class User extends React.Component {
-    render(){
-        return (
-        <div>I am user {this.props.user.name}
-        
-        </div>
-        
-        ) 
-    }
+const User = props => {
+    return (
+    <div className="userInfo"> 
+        <h2> {props.user.name}</h2>
+        <img width="200" src={props.user.avatar_url} />
+        <h3>Total Repos: {props.user.public_repos}</h3>
+        <h3>Total Followers: {props.user.followers}</h3>
+
+    </div>
+    )
 }
-export default User
+export default User 
