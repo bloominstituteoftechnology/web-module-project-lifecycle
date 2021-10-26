37df7f8288a9d074fb8e0cea9acd4e2d4378fe12
Justin Peisker <justinpeisker@gmail.com>
2021-10-26 14:58:30 -0700

    more progeress

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ class App extends React.Component {
     login: '',
     public_repos: '',
     followers: '',
+    input: ''
 
   }
 
   componentDidMount () {
-    axios.get("https://api.github.com/users/justinpeisker")
+    axios.get(`https://api.github.com/users/justinpeisker`)
       .then(res => {
         console.log(res);
         this.setState({
@@ -30,13 +31,50 @@ class App extends React.Component {
         console.error(err);
       })
   }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if(this.state.input === 'justinpeisker') {
+  //     axios.get("https://api.github.com/users/justinpeisker/followers")
+  //       .then(res => {
+  //         this.setState({
+  //           ...this.state,
+  //           avatar_url: res.data.avatar_url,
+  //           name: res.data.name
+  //         })
+  //       })
+  //   }
+  // }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      input: e.target.value
+    })
+    console.log(e.target.value)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.input}`)
+      .then(res => {
+        // console.log(res);
+        this.setState({
+          ...this.state,
+          avatar_url: res.data.avatar_url,
+          name: res.data.name,
+          login: res.data.login,
+          public_repos: res.data.public_repos,
+          followers: res.data.followers
+        })
+      })
+  }
   render() {
     return(
       <div>
         <h1> GitHub Info </h1>
-        <form>
-          <input />
-          <button>Search</button>    
+        <form >
+          <input value= {this.state.input} onChange={this.handleChange}/>
+          <button onClick={this.handleSubmit}>Search</button>    
         </form>
         <div className= 'main-profile'>
           <img src= {this.state.avatar_url} alt='' />
@@ -46,6 +84,7 @@ class App extends React.Component {
           <h4> Total Followers: {this.state.followers} </h4>
         </div>
         <div className= 'followers'>
+          <h2>FOLLOWERS:</h2>
           <img src= '' alt= '' />
           <h3> Name: </h3>
 
