8e118b05f1dd9be691b590c4365ebb39a85659c3
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-26 20:28:44 -0400

    props progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,99 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 import FollowerList from "./components/FollowerList";
 import User from "./components/User";
 
 class App extends React.Component {
+  state = {
+    input: "",
+    login: "",
+    id: "",
+    name: "",
+    avatar_url: "",
+    public_repos: "",
+    followers: "",
+    followers_list: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/tinaxgao")
+      .then((resp) => {
+        this.setState({
+          ...this.state,
+          login: resp.data.login,
+          id: resp.data.id,
+          name: resp.data.name,
+          avatar_url: resp.data.avatar_url,
+          public_repos: resp.data.public_repos,
+          followers: resp.data.followers,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios
+      .get("https://api.github.com/users/tinaxgao/followers")
+      .then((resp) => {
+        this.setState({
+          ...this.state,
+          followers_list: resp.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      input: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.input}`)
+      .then((resp) => {
+        this.setState({
+          ...this.state,
+          login: resp.data.login,
+          id: resp.data.id,
+          name: resp.data.name,
+          avatar_url: resp.data.avatar_url,
+          public_repos: resp.data.public_repos,
+          followers: resp.data.followers,
+        });
+      }).catch(err => {console.log(err)});
+      axios
+      .get(`https://api.github.com/users/${this.state.input}/followers`)
+      .then((resp) => {
+        this.setState({
+          ...this.state,
+          followers_list: resp.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div>
         <h1>Github Card</h1>
         <form>
-          <input placeholder="GitHub Username" />
-          <button>Search</button>
+          <input
+            placeholder="GitHub Username"
+            value={this.state.input}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}>Search</button>
         </form>
-        <User />
+        <User name={this.state.name} login={this.state.login} pic={this.state.avatar_url} repos={this.state.public_repos} followers={this.state.followers} />
         <h3>Followers</h3>
         <FollowerList />
       </div>
--- src/components/User.js
+++ src/components/User.js
@@ -4,9 +4,9 @@ class User extends React.Component {
   render() {
     return (
       <div className="user-profile">
-        <img src="https://avatars.githubusercontent.com/u/89364961?v=4" />
-        <h2>Tina Gao</h2>
-        <h5>username: tinaxgao</h5>
+        <img src={this.props.pic} />
+        <h2>{this.props.name}</h2>
+        <h5>username: {this.props.login}</h5>
         <h4>Repos: 37</h4>
         <h4>Followers: 18</h4>
       </div>
