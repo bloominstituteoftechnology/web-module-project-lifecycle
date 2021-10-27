50d37812a010bfbccacf5c9e467c9aef3a797324
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-10-26 19:12:15 -0700

    setting up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return(<div>
-      Github Card
-    </div>);
-  }
+class App extends React.Component
+{
+    render()
+    {
+        return (<div>
+            Github Card
+        </div>);
+    }
 }
 
 export default App;
