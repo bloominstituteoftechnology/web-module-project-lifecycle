a99c84812cf27d33cbe111772f9fe0426c534d08
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-10-26 18:54:48 -0700

    build the DOM necessary to display all application information

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React from 'react'
+import axios from 'axios'
+
+import FollowerList from './components/FollowerList'
+
+import './App.css'
 
 class App extends React.Component {
+  state = {
+    user:[],
+    input: ""
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/beatlesm')
+        .then(res=> {
+          // debugger
+          this.setState({
+            ...this.state,
+            user: res.data
+          });  
+        })
+        .catch(err=> {
+            console.log(err);
+        });
+}
+
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+      <div className="container">      
+        <h1> GITHUB INFO</h1>
+        <form className="form"> 
+          <input />
+          <button >Search</button>
+          <div className="user">
+              {console.log('this.state', this.state.user.avatar_url)}
+              <img src={this.state.user.avatar_url} alt={this.state.user.login} />
+              <div className= "user-info">
+                  <h2>{this.state.user.name}</h2>
+                  <h3>TOTAL REPOS: {this.state.user.public_repos} </h3>
+                  <h3>TOTAL Followers: {this.state.user.followers} </h3>
+              </div>  
+              {/* <FollowerList />              */}
+          </div>
+        </form>           
+      </div>);
   }
 }
 
