bdb358209eefd7cbeea06a7721865e70296754cb
mspicola <mspicola@protonmail.com>
2021-10-27 00:54:22 -0400

    setting up imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import User from './components/User';
+import FollowerList from './components/FollowerList'
+import axios from 'axios';
+
 
 class App extends React.Component {
-  render() {
-    return(<div>
-      Github Card
-    </div>);
+  state = {
+    userProfile: {},
+    followers: [],
+    input: ''
   }
-}
+
+  handleChange = evt => {
+    this.setState({
+      ...this.state,
+      input: evt.target.value
+    })
+  }
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.input}`)
+      .then(resp => {
+        this.setState({
+          ...this.state,
+          userProfile: resp.data
+        })
+      })
+  }
 
 export default App;
