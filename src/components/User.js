acd667554ce7a51cb7d2c6b301852486bcfc0b5a
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-26 21:43:03 -0400

    MVP

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,7 @@ import React from 'react';
 const User = props => {
     return (
     <div className="userInfo"> 
-
+    
         <h2> {props.user.name}</h2>
         <h4>{props.user.login}</h4>
         <img width="200" src={props.user.avatar_url} alt="User img" />
