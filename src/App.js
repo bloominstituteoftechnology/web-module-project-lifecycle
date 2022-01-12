f27a2cd785212505c6a19ea3541aee1d4ad48ff8
marcel <marcel_rodrgez@yahoo.com>
2022-01-12 01:30:53 -0500

    Finished ui and most of the logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,83 @@
+import axios from 'axios';
 import React from 'react';
+import './index.css'
 
 class App extends React.Component {
+  state ={
+    currentUser: 'Marcel-rodriguez',
+    followerList: [],
+    avatar: '',
+    username: '',
+    repos: '',
+    followers: '',
+    following: '',
+  }
+
+  componentDidMount(){
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+    .then(resp => {
+      console.log(resp.data)
+      this.setState({
+        ...this.state,
+        currentUser: resp.data.avatar_url,
+        avatar: resp.data.avatar_url,
+        username: resp.data.login,
+        followers: resp.data.followers,
+        following: resp.data.following,
+        repos: resp.data.public_repos,
+      })
+    })
+    .catch(err => {
+      console.error(err)
+    })
+
+    axios.get(`https://api.github.com/users/${this.state.currentUser}/followers`)
+    .then(resp => {
+      console.log(resp.data)
+      this.setState({
+        ...this.state,
+        followerList: resp.data
+      })
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }
+
   render() {
+    console.log('User Image: ', this.state.currentUser)
+    console.log('Follower List: ', this.state.followerList)
     return(<div>
-      Github Card
+      <div className='search-bar'>
+      <h1>Github Info</h1>
+      <form>
+      <input />
+      <button>Search</button>
+      </form>
+      </div>
+      <div className='main-info'>
+        <img src={this.state.avatar} alt={this.state.avatar}/>
+        <span className='main-info-data'>
+        <h2>{this.state.username}</h2>
+        <p>Repos: {this.state.repos}</p>
+        <p>Following: {this.state.following}</p>
+        <p>Followers: {this.state.followers}</p>
+        </span>
+      </div>
+      <hr />
+      <div className='bottom'>
+        <h2>Followers</h2>
+        <div className='card-area'>
+          {
+            this.state.followerList.map(follower => {
+              return <div>
+                <img src={follower.avatar_url} alt={follower.id}/>
+                <h3>{follower.login}</h3>
+              </div>
+            })
+          }
+        </div>
+      </div>
     </div>);
   }
 }
