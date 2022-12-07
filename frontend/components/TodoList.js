ede8a3ea55a59859e88b5f7f2aa45134e9dcd5a8
Terri Archer <territangela@gmail.com>
2022-12-07 13:07:46 -0500

    completed coursework

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
+  
+  
   render() {
-    return null
+    return (
+      <>
+        {this.props.todo.map((task) => {
+          return <Todo task={task} key={task.id} toggleCompleted={this.props.toggleCompleted}/>
+          
+        })}
+      </>
+    )
   }
 }
