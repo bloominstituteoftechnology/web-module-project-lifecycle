6d3df36f121bdd0a5a6d5c9f503427c96740bf73
james starling <e.s.gbeast@gmail.com>
2022-12-17 11:38:46 -0500

    all together

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
-    return null
+    return (
+      <form onSubmit={this.props.handleSubmit}>
+        <input 
+          type='text' 
+          onChange={this.props.inputChange}
+          value={this.props.inputValue} />
+        <button>Add me!</button>
+      </form>
+    )
   }
 }
