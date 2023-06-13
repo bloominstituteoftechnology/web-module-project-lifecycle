424f92c831bbc249f7281d3685a3c0b1193a0502
DJ1 <jimmynoble604@gmail.com>
2023-06-13 16:20:12 -0400

    App.js complete

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,73 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import TodoList from "./TodoList"
+import axios from "axios" 
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+    error: "",
+    todonameinput: "",
+  }
+  fetchalltodos = () => {
+    axios.get(URL)
+    .then(res => {
+      this.setState({...this.state, todos: res.data.data})
+    })
+    .catch(err => {this.setState({...this.state, error: err.response.data.message})
+    })
+  };
+  componentDidMount(){
+    this.fetchalltodos()
+  }
+  oninputchange = evt => {
+    const {value} = evt.target
+    this.setState({...this.state, todoname: value})
+  }
+  postnewtodo = () => {
+    axios.post(URL, {name: this.state.todonameinput})
+    .then(res => {
+      this.fetchalltodos();
+      this.setState({...this.state, todonameinput: "" })
+    })
+    .catch(err => {this.setState({...this.state, error: err.response.data.message})
+   })
+  }
+  onsubmit = (e) => {
+    e.preventDefault();
+    this.postnewtodo()
+  }
+  clearcompleted = (e) => {
+
+  }
+  togglecompleted = id => {
+    axios.patch(`${URL}/${id}`)
+    .then(res => {
+      this.setState({...this.state, todos: [this.state.todos.map(td => {
+        if (td.id !== id) return td
+        else {
+          return res.dtat.data
+        }
+      })]})
+    })
+    .catch(err => {this.setState({...this.state, error: err.response.data.message})})
+  }
+  onclickhandler = id => e => {
+    this.togglecompleted(id)
+  }
   render() {
-    return null
+    return (
+      <>
+      <h2> error: {this.state.error}</h2>
+      <TodoList todos ={this.state.todos} onclickhandler={this.onclickhandler} />
+      <form onSubmit={this.onsubmit}>
+        <input value={this.state.todonameinput} onChange={this.oninputchange} type="text" />
+        <button>ADD TODO</button>
+        <button onClick={this.clearcompleted}>CLEAR SPACE COMPLETED</button>
+      </form>
+      </>
+    )
   }
 }
