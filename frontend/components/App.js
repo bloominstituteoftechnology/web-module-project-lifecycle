37c57638120b85ad2e6918c32877cb573d533e18
Kate Ridgle <kroo.2394@gmail.com>
2023-07-08 23:19:06 -0700

    added form and divs and inputs

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
-export default class App extends React.Component {
-  render() {
-    return null
+export default class App extends React.Component { 
+
+  state = {
+    todos: [],
+    error: '',
+    todoNameInput: '',
   }
+
+  onNameChange = evt => {
+    const {value} = evt.target
+    this.setState({...this.state, todoNameInput: value})
+  }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({...this.state, todos: res.data.data})
+      })
+      .catch(err => {
+
+      })
+  }
+  componentDidMount(){
+  this.fetchAllTodos()
+ }
+  
+ 
+ render() {
+    return (
+
+      <div>
+        <div id="error">Error: {this.state.error}</div>
+        <div id="todos">
+            <h2>Todos:</h2>
+            {
+          this.state.todos.map(td=> {
+            return <div key={td.id}>{td.name}</div>
+          })
+        }
+
+        </div>
+        <form id="todoForm">
+          <input onchange={this.onNameChange} value={this.state.todoNameInput} type="text" placeholder="type todo"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
+  }
+
 }
