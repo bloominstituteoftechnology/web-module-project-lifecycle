97f228967716c6567377bd59bb0c5c4dfe9f5298
Jomar Banting <shadowlover.05@gmail.com>
2023-07-16 19:13:12 -0400

    Rendered the data fetched from the api endpoint

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import axios from "axios"
+import TodoList from "./TodoList"
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      todos: [],
+      error: {
+        message: "",
+        status: false
+      },
+    }
+  }
+
+    fetchAllTodos = () => {
+      axios.get(URL)
+      .then(res => {
+        this.setState({
+          ...this.state, todos: res.data.data
+        })
+      })
+      .catch(err => {
+        this.setState({
+          ...this.state, error: {
+            message: err.response.data.message,
+            status: !this.state.error.status
+          }
+        })
+      })
+    }
+
+    componentDidMount(){
+      this.fetchAllTodos();
+    }
+
   render() {
-    return null
+    const { todos } = this.state;
+    return (
+      <div>
+        {this.state.error.status? <div id="error">Error: {this.state.error.message}</div> : <></>}
+        <h2>Todos:</h2>
+        <TodoList data={todos}/>
+      </div>
+    )
   }
 }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
-    return null
+    return (
+      <li>{this.props.data.name}{this.props.data.completed? <span> --- TASK COMPLETED</span> : <span></span>}</li>
+    )
   }
 }
